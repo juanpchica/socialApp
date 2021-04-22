4e6bd64709d345136c54a63f7cd59ff4d63a1cde
juanpchica <juanpchica@hotmail.com>
2021-04-22 09:39:44 -0500

    MONGODB Connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+const { MONGODB } = require("./config");
 
 const typeDefs = gql`
   type Query {
@@ -18,6 +20,12 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`);
-});
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`MONGODB Connected`);
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
