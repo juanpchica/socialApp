026188c44cb661d92995c6b3691c35036b99befd
juanpchica <juanpchica@hotmail.com>
2021-04-25 14:34:35 -0500

    getPost query added

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -2,10 +2,22 @@ const Post = require("../../models/Post");
 
 module.exports = {
   Query: {
-    async getPost() {
+    async getPosts() {
       try {
         const posts = await Post.find();
         return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
       } catch (err) {
         throw new Error(err);
       }
