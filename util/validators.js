db796bfd5e4d3a305c887ff84e7ab827ff43d0ed
juanpchica <juanpchica@hotmail.com>
2021-04-25 12:02:00 -0500

    Register input validator added

---
+++ util/validators.js
@@ -0,0 +1,31 @@
+module.exports.validateRegisterInput = (
+  username,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+
+  if (email.trim() === "") {
+    errors.email = "Email must not be empty";
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = "Email must be a valid email address";
+    }
+  }
+
+  if (password === "") {
+    errors.password = "Password must not be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  return {
+    errors,
+    validate: Object.keys.length < 1,
+  };
+};
