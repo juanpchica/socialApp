d858dda0e0a398ef80ebf778b734557787158296
juanpchica <juanpchica@hotmail.com>
2021-04-26 11:52:03 -0500

    Git query posts added graphql apollo client

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
 const Home = () => {
+  const {
+    loading,
+    error,
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POSTS_QUERY);
+  console.log(posts);
   return <div>Home</div>;
 };
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
