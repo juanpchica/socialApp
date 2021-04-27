0fe91e57117762a4f270185db2d488f09a7eac71
juanpchica <juanpchica@hotmail.com>
2021-04-27 16:50:04 -0500

    Handle errors server added for registration form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,14 @@
 import { useMutation, gql } from "@apollo/client";
 import React, { useState } from "react";
 import { Button, Form } from "semantic-ui-react";
-const Register = () => {
+const Register = (props) => {
   const [values, setValues] = useState({
     username: "",
     email: "",
     password: "",
     confirmPassword: "",
   });
-
+  const [errors, setErrors] = useState({});
   const onChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
@@ -16,6 +16,10 @@ const Register = () => {
   const [addUser, { loading }] = useMutation(REGISTER_USER, {
     update(_, result) {
       console.log(result);
+      props.history.push("/");
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
     },
     variables: values,
   });
@@ -26,37 +30,57 @@ const Register = () => {
   };
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Form.Input
-        label="Username"
-        placeholder="Username..."
-        name="username"
-        value={values.username}
-        onChange={onChange}
-      />
-      <Form.Input
-        label="Email"
-        name="email"
-        placeholder="Email..."
-        value={values.email}
-        onChange={onChange}
-      />
-      <Form.Input
-        label="Password"
-        name="password"
-        placeholder="Password..."
-        value={values.password}
-        onChange={onChange}
-      />
-      <Form.Input
-        label="Confirm Password"
-        name="confirmPassword"
-        placeholder="Confirm Password..."
-        value={values.confirmPassword}
-        onChange={onChange}
-      />
-      <Button type="submit">Sign Up</Button>
-    </Form>
+    <div className="form-container">
+      <h1>Register</h1>
+      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
+        <Form.Input
+          label="Username"
+          placeholder="Username..."
+          type="text"
+          name="username"
+          value={values.username}
+          onChange={onChange}
+          error={errors.username ? true : false}
+        />
+        <Form.Input
+          label="Email"
+          name="email"
+          type="email"
+          placeholder="Email..."
+          value={values.email}
+          onChange={onChange}
+          error={errors.email ? true : false}
+        />
+        <Form.Input
+          label="Password"
+          name="password"
+          type="password"
+          placeholder="Password..."
+          value={values.password}
+          onChange={onChange}
+          error={errors.password ? true : false}
+        />
+        <Form.Input
+          label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm Password..."
+          value={values.confirmPassword}
+          onChange={onChange}
+          error={errors.confirmPassword ? true : false}
+        />
+        <Button type="submit">Sign Up</Button>
+      </Form>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
