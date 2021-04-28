9554ca7a9a27c1c80a46570a147093b1c9abb4a9
juanpchica <juanpchica@hotmail.com>
2021-04-28 11:34:54 -0500

    Authorization token send with grapql link

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,13 +1,26 @@
 import React from "react";
 import App from "./App";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { ApolloProvider } from "@apollo/client/react";
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:5000",
-  cache: new InMemoryCache(),
 });
 
+const authLink = setContext((request, previousContext) => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 const ApolloProvide = () => {
   return (
     <ApolloProvider client={client}>
