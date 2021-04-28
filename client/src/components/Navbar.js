28bb57bb6a7aa7eae8d532f5533b5913f811c671
juanpchica <juanpchica@hotmail.com>
2021-04-28 10:05:00 -0500

    Navbar validate if user exist

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
-
+import { AuthContext } from "../context/auth";
 function Navbar() {
+  const { user, logout } = useContext(AuthContext);
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   const pathname = window.location.pathname;
@@ -10,7 +11,15 @@ function Navbar() {
   const path = pathname === "/" ? "home" : pathname.substr(1);
   const [activeItem, setActiveItem] = useState(path);
 
-  return (
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item name={user.username} active as={Link} to="/" />
+
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
@@ -38,6 +47,8 @@ function Navbar() {
       </Menu.Menu>
     </Menu>
   );
+
+  return menuBar;
 }
 
 export default Navbar;
