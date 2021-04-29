29b20c5eda738a21a9560031534dca81713969f0
juanpchica <juanpchica@hotmail.com>
2021-04-29 14:32:44 -0500

    PORT const added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config");
 
 const pubsub = new PubSub();
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -17,8 +18,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log(`MONGODB Connected`);
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.log(err);
   });
